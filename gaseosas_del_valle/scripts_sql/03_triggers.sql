409b7eb1d11a72e8acf1625dad217209e0eecd12
“kevinchavezg” <kevinchavezg@gmail.com>
2026-08-13 08:20:55 -0600

    (scripts_sql) primer triger

---
+++ gaseosas_del_valle/scripts_sql/03_triggers.sql
@@ -0,0 +1,19 @@
+USE gaseosas_del_valle;
+
+DELIMITER $$
+
+DROP TRIGGER IF EXISTS tr_actualizar_stock$$
+CREATE TRIGGER tr_actualizar_stock
+AFTER INSERT ON detalle_pedido
+FOR EACH ROW
+BEGIN
+    UPDATE productos
+    SET stock_actual = stock_actual - NEW.cantidad
+    WHERE id_producto = NEW.id_producto;
+    
+    UPDATE pedidos
+    SET total_sin_iva = (SELECT IFNULL(SUM(subtotal),0) FROM detalle_pedido WHERE id_pedido = NEW.id_pedido),
+        total_con_iva = fn_calcular_total_con_iva(NEW.id_pedido)
+    WHERE id_pedido = NEW.id_pedido;
+END$$
+
