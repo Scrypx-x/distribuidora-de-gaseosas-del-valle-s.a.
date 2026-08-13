4a34f1e49c6def546161339bb628b964236cfc39
“kevinchavezg” <kevinchavezg@gmail.com>
2026-08-13 08:48:52 -0600

    (scripts_sql) trigers

--- gaseosas_del_valle/scripts_sql/03_triggers.sql
+++ gaseosas_del_valle/scripts_sql/03_triggers.sql
@@ -17,3 +17,16 @@ BEGIN
     WHERE id_pedido = NEW.id_pedido;
 END$$
 
+DROP TRIGGER IF EXISTS tr_auditar_cambio_precio$$
+CREATE TRIGGER tr_auditar_cambio_precio
+AFTER UPDATE ON productos
+FOR EACH ROW
+BEGIN
+    IF OLD.precio <> NEW.precio THEN
+        INSERT INTO auditoria_precios (id_producto, precio_anterior, precio_nuevo)
+        VALUES (OLD.id_producto, OLD.precio, NEW.precio);
+    END IF;
+END$$
+
+DELIMITER ;
+
